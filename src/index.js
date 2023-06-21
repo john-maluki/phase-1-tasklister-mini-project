d18684a3acbc38dfc44899f8ee8c7fe0dad2186f
john <johnmaluki58@gmail.com>
2023-06-21 19:50:35 +0300

    Complete assignment on TaskLister Mini-Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const formNode = document.querySelector("#create-task-form");
+  formNode.addEventListener("submit", buildTaskLister);
 });
+
+const buildTaskLister = (e) => {
+  e.preventDefault();
+  const inputNode = document.querySelector("#new-task-description");
+  const tasksNode = document.querySelector("#tasks");
+
+  const buttonNode = createButton();
+  const listNode = createLI(inputNode.value, buttonNode);
+  tasksNode.append(listNode);
+};
+
+const deleteTask = (e) => {
+  const liNode = e.target.parentNode;
+  liNode.remove();
+};
+
+const createLI = (task, button) => {
+  const listNode = document.createElement("li");
+  listNode.textContent = task;
+  listNode.append(button);
+  return listNode;
+};
+
+const createButton = () => {
+  const buttonNode = document.createElement("button");
+  buttonNode.textContent = "X";
+  buttonNode.id = "delete-button";
+  buttonNode.addEventListener("click", deleteTask);
+  return buttonNode;
+};
